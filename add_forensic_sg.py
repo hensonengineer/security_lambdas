faf08281da4c954b58d7695bea6761340399ca08
Chris Henson <hensonengineer@gmail.com>
2023-11-01 15:22:01 -0600

    Update add_forensic_sg.py

--- add_forensic_sg.py
+++ add_forensic_sg.py
@@ -5,26 +5,24 @@
 
 import boto3
 
-
 def lambda_handler(event, context):
-    # Retrieve the instance ID from the event data
-    # uncomment the below when passing in the Instance ID from a cloudwatch event
-    #instance_id = event['instance_id']
-    
-    # hard code yourself for testing
-    instance_id = "<your test instanceId>"
-    
-    #pre-built / existing forensic security group that will be added
-    security_group_id = "<your forensic sg id>"
-
-    # Create an EC2 client
-    ec2 = boto3.client('ec2')
-
-    # Attach the forensic security group to an EC2 instance
-    response = ec2.modify_instance_attribute(
-        InstanceId=instance_id,
-        Groups=[security_group_id]
-    )
-
-    #print(f"Security group {security_group_id} attached to instance {instance_id}")
+    try:
+        # Retrieve the instance ID from the event data
+        instance_id = event['instance_id']
+        
+        # Pre-built / existing forensic security group that will be added
+        security_group_id = "<your forensic sg id>"
+        
+        # Create an EC2 client
+        ec2 = boto3.client('ec2')
+        
+        # Attach the forensic security group to an EC2 instance
+        response = ec2.modify_instance_attribute(
+            InstanceId=instance_id,
+            Groups=[security_group_id]
+        )
+        
+        return f"Security group {security_group_id} attached to instance {instance_id}"
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
 
